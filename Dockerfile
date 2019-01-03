250a69888ac99c55ab6da485fb52d8dc3ab7e963
Justin Chase <jchase@structural.com>
2019-01-03 14:55:58 -0600

    Add aws-sdk into loadable place for tasks

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM node:8.10
 
+WORKDIR /var
+COPY package.json package-lock.json /var/
+RUN npm i
+
 WORKDIR /var/app
 COPY src/app/*.json /var/app/
 RUN npm i
--- src/tasks/example/index.js
+++ src/tasks/example/index.js
@@ -1,3 +1,5 @@
+require('aws-sdk')
+
 exports.handler = function (event, context) {
   return new Promise((resolve, reject) => {
     console.log("hello handler!")
